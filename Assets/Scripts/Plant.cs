0c57f28a0f18db3e72b276c95224c6239e4183f3
Colin Spiridonov <colinspiri@gmail.com>
2022-11-16 00:25:24 -0800

    working on plant cover

---
+++ Assets/Scripts/Plant.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plant : MonoBehaviour {
+    public float loudness;
+
+    private void Rustle() {
+        // play audio TODO
+        
+        // report sound
+        TorbalanSenses.Instance.ReportSound(FirstPersonController.Instance.transform.position, loudness);
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if (other.gameObject == FirstPersonController.Instance.gameObject) {
+            if (FirstPersonController.Instance.GetMoveState != FirstPersonController.MoveState.Still) {
+                Rustle();
+            }
+        }
+    }
+    
+    private void OnDrawGizmosSelected() {
+        // loudness radius
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, loudness);
+    }
+}
