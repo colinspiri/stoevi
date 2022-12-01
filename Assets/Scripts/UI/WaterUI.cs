33589e0e2544b4ddd9ace06b3270835fba6511ad
Colin Spiridonov <colinspiri@gmail.com>
2022-11-30 23:11:29 -0800

    made water UI appear only when watering

--- Assets/Scripts/UI/WaterUI.cs
+++ Assets/Scripts/UI/WaterUI.cs
@@ -9,12 +9,28 @@ public class WaterUI : MonoBehaviour
 {
     // components
     public Slider waterSlider;
+    private CanvasGroup canvasGroup;
+    
+    // constants
+    public float fadeInTime;
+    public float valueLerpTime;
+    public float waitTime;
+    public float fadeOutTime;
 
     // Start is called before the first frame update
     void Start() {
+        canvasGroup = waterSlider.GetComponent<CanvasGroup>();
         waterSlider.value = 1;
+        canvasGroup.alpha = 0;
+        
         GameManager.Instance.onWaterValueChange.AddListener(waterValue => {
-            waterSlider.DOValue(waterValue, 0.5f);
+            canvasGroup.DOFade(1, fadeInTime);
+            Sequence sequence = DOTween.Sequence();
+            sequence.Append(waterSlider.DOValue(waterValue, valueLerpTime));
+            sequence.AppendInterval(waitTime);
+            sequence.Append(canvasGroup.DOFade(0, fadeOutTime));
         });
     }
+    
+    
 }
