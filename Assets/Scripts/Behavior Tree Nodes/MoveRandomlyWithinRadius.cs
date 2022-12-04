9c6519ad888ce74bf54f4febdaa986749a71caa1
Colin Spiridonov <colinspiri@gmail.com>
2022-12-03 19:23:23 -0800

    fixed sheep movement bug

--- Assets/Scripts/Behavior Tree Nodes/MoveRandomlyWithinRadius.cs
+++ Assets/Scripts/Behavior Tree Nodes/MoveRandomlyWithinRadius.cs
@@ -13,46 +13,60 @@ namespace BehaviorDesigner.Runtime.Tasks.Movement {
 
         public SharedFloat minPauseDuration;
         public SharedFloat maxPauseDuration;
+        
+        private enum State { Moving, Paused }
+        private State currentState;
 
-        private float timer;
-        private float pauseTime;
-        private float destinationReachTime;
+        private float exitTimer; // counts up
+        private float pauseTimer; // counts down
 
         public override void OnStart() {
             base.OnStart();
 
-            SetDestination(GetRandomPosition());
-            timer = 0;
-            destinationReachTime = -1;
+            if (maxPauseDuration.Value > 0) StartPause();
+            else StartMoving();
         }
 
 
         public override TaskStatus OnUpdate() {
             if (moveForever.Value == false) {
-                timer += Time.deltaTime;
-                if (timer >= maximumTime.Value) return TaskStatus.Success;
+                exitTimer += Time.deltaTime;
+                if (exitTimer >= maximumTime.Value) return TaskStatus.Success;
             }
 
-            if (HasArrived()) {
-                if (maxPauseDuration.Value > 0) {
-                    if (destinationReachTime == -1) {
-                        destinationReachTime = Time.time;
-                        pauseTime = Random.Range(minPauseDuration.Value, maxPauseDuration.Value);
-                    }
-                    if (destinationReachTime + pauseTime <= Time.time) {
-                        SetDestination(GetRandomPosition());
-                        destinationReachTime = -1;
-                    }
+            if (currentState == State.Moving) {
+                if (HasArrived()) {
+                    if (maxPauseDuration.Value > 0) StartPause();
+                    else StartMoving();
+                }
+            }
+
+            if (currentState == State.Paused) {
+                pauseTimer -= Time.deltaTime;
+                if (pauseTimer <= 0) {
+                    StartMoving();
                 }
             }
             
             return TaskStatus.Running;
         }
 
+        private void StartMoving() {
+            currentState = State.Moving;
+
+            SetDestination(GetRandomPosition());
+        }
+
+        private void StartPause() {
+            currentState = State.Paused;
+
+            pauseTimer = Random.Range(minPauseDuration.Value, maxPauseDuration.Value);
+        }
+
         private Vector3 GetRandomPosition() {
             Vector2 randForward = Random.insideUnitCircle.normalized;
             float randDist = Random.value * radius.Value;
-            Vector3 targetPos = center.Value + (new Vector3(randForward.x, center.Value.y, randForward.y) * randDist);
+            Vector3 targetPos = center.Value + (new Vector3(randForward.x, 0, randForward.y) * randDist);
             return targetPos;
         }
     }
