d0cd154c37c6f3c978e810d235691210aeafefbd
Colin Spiridonov <colinspiri@gmail.com>
2022-12-22 14:44:55 -0800

    added running animation

--- Assets/Scripts/TorbalanAnimatorController.cs
+++ Assets/Scripts/TorbalanAnimatorController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using BehaviorDesigner.Runtime;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -10,12 +11,17 @@ public class TorbalanAnimatorController : MonoBehaviour {
     
     // constants
     public float walkingSpeedThreshold;
+    public float runningSpeedThreshold;
 
     // Update is called once per frame
     void Update() {
         float currentSpeed = agent.velocity.magnitude;
+        
         bool walking = currentSpeed > walkingSpeedThreshold;
         // Debug.Log("walking " + walking + " with speed = " + currentSpeed);
         animator.SetBool("walking", walking);
+
+        bool running = currentSpeed > runningSpeedThreshold;
+        animator.SetBool("running", running);
     }
 }
