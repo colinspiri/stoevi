aa88522ff8517a5e1f85b7b90e11029ea8272310
Colin Spiridonov <colinspiri@gmail.com>
2022-12-25 10:02:38 -0800

    added gate

---
+++ Assets/Scripts/Interactable/Gate.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : Interactable {
+    
+    public override void Interact() {
+        GameManager.Instance.GameOver(true);
+    }
+
+    public override string GetObjectName() {
+        return "gate";
+    }
+
+    public override string GetObjectDescription() {
+        return "";
+    }
+
+    public override string GetButtonPrompt() {
+        return "E to go home";
+    }
+}
