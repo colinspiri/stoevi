4e9c6c99967bbdb9287497f27f52152a8c95b860
Colin Spiridonov <colinspiri@gmail.com>
2023-01-11 14:31:24 -0800

    refactored some stuff into Util

--- Assets/Scripts/Editor/DropObjectsEditorWindow.cs
+++ Assets/Scripts/Editor/DropObjectsEditorWindow.cs
@@ -3,8 +3,6 @@ using UnityEditor;
  
 public class DropObjectsEditorWindow : EditorWindow
 {
-    private bool m_bAlignRotations = true;
- 
     // Add a menu item
     [ MenuItem("Window/Drop Object(s)") ]
  
@@ -32,9 +30,6 @@ public class DropObjectsEditorWindow : EditorWindow
             DropObjects("Center");
         }
         EditorGUILayout.EndHorizontal();
- 
-        // Add checkbox for rotation alignment
-        m_bAlignRotations = EditorGUILayout.ToggleLeft( "Align Rotations", m_bAlignRotations );
 
         if (GUILayout.Button("Raise and level object(s)")) {
             RaiseAndLevel();
@@ -50,74 +45,20 @@ public class DropObjectsEditorWindow : EditorWindow
                 continue;
             
             // reset rotation and raise above terrain
-            go.transform.rotation = Quaternion.identity;
+            go.transform.rotation = Quaternion.Euler(0, go.transform.eulerAngles.y, 0);
             go.transform.Translate(2 * Vector3.up);
         }
     }
  
-    void DropObjects(string Method)
-    {
+    private void DropObjects(string method) {
         Undo.RecordObjects( Selection.transforms, "Drop Objects" );
- 
-        for ( int i = 0; i < Selection.transforms.Length; i++ )
-        {
-            GameObject go = Selection.transforms[i].gameObject;
+
+        foreach (var selectedTransform in Selection.transforms) {
+            GameObject go = selectedTransform.gameObject;
             if ( !go )
                 continue;
 
-            // Cast a ray and get all hits
-            RaycastHit[] rgHits = Physics.RaycastAll( go.transform.position, -Vector3.up, Mathf.Infinity );
- 
-            // We can assume we did not hit the current game object, since a ray cast from within the collider will implicitly ignore that collision
-            int iBestHit = -1;
-            float flDistanceToClosestCollider = Mathf.Infinity;
-            for( int iHit = 0; iHit < rgHits.Length; ++iHit )
-            {
-                RaycastHit CurHit = rgHits[ iHit ];
- 
-                // Assume we want the closest hit
-                if ( CurHit.distance > flDistanceToClosestCollider )
-                    continue;
- 
-                // Cache off the best hit
-                iBestHit = iHit;
-                flDistanceToClosestCollider = CurHit.distance;
-            }
- 
-            // Did we find something?
-            if ( iBestHit < 0 )
-            {
-                Debug.LogWarning( "Failed to find an object on which to place the game object " + go.name + "." );
-                continue;
-            }
- 
-            // Grab the best hit
-            RaycastHit BestHit = rgHits[ iBestHit ];
-            
-            // calculate offset based on settings
-            float yOffset = 0f;
-            Bounds bounds = go.GetComponent<Renderer>().bounds;
-            switch (Method)
-            {
-                case "Bottom":
-                    yOffset = go.transform.position.y - bounds.min.y;
-                    break;
-                case "Origin":
-                    yOffset = 0f;
-                    break;
-                case "Center":
-                    yOffset = bounds.center.y - go.transform.position.y;
-                    break;
-            }
-
-            // Set position
-            go.transform.position = new Vector3( BestHit.point.x, BestHit.point.y + yOffset, BestHit.point.z );
- 
-            // Set rotation
-            if ( m_bAlignRotations )
-            {
-                go.transform.rotation = Quaternion.FromToRotation( Vector3.up, BestHit.normal );
-            }
+            Util.PlaceGameObjectOnTerrain(go, method);
         }
     }
 }
--- Assets/Scripts/Util.cs
+++ Assets/Scripts/Util.cs
@@ -17,4 +17,76 @@ public static class Util {
 
         return randomPoint;
     }
+
+    public static void PlaceGameObjectOnTerrain(GameObject go, string method) {
+        // Cast a ray and get all hits
+        RaycastHit[] rgHits = Physics.RaycastAll( go.transform.position, -Vector3.up, Mathf.Infinity );
+
+        // We can assume we did not hit the current game object, since a ray cast from within the collider will implicitly ignore that collision
+        int iBestHit = -1;
+        float flDistanceToClosestCollider = Mathf.Infinity;
+        for( int iHit = 0; iHit < rgHits.Length; ++iHit )
+        {
+            RaycastHit CurHit = rgHits[ iHit ];
+
+            // Assume we want the closest hit
+            if ( CurHit.distance > flDistanceToClosestCollider )
+                continue;
+
+            // Cache off the best hit
+            iBestHit = iHit;
+            flDistanceToClosestCollider = CurHit.distance;
+        }
+
+        // Did we find something?
+        if ( iBestHit < 0 )
+        {
+            Debug.LogWarning( "Failed to find an object on which to place the game object " + go.name + "." );
+            return;
+        }
+
+        // Grab the best hit
+        RaycastHit BestHit = rgHits[ iBestHit ];
+        
+        // calculate offset based on settings
+        float yOffset = 0f;
+        Bounds bounds = GetBoundsForGameObjectHierarchy(go);
+        switch (method)
+        {
+            case "Bottom":
+                yOffset = go.transform.position.y - bounds.min.y;
+                break;
+            case "Origin":
+                yOffset = 0f;
+                break;
+            case "Center":
+                yOffset = bounds.center.y - go.transform.position.y;
+                break;
+        }
+
+        // Set position
+        go.transform.position = new Vector3( BestHit.point.x, BestHit.point.y + yOffset, BestHit.point.z );
+        
+        // Set rotation
+        go.transform.rotation = Quaternion.FromToRotation( Vector3.up, BestHit.normal );
+    }
+    
+    public static Bounds GetBoundsForGameObjectHierarchy( GameObject go )
+    {
+        Bounds bounds = new Bounds();
+        bounds.center = go.transform.position;
+        bounds.extents = Vector3.zero;
+ 
+        // Deal with parent and all descendents (GetComponentsInChildren() gets everything)
+        Renderer[] rgDescendentRenderers = go.GetComponentsInChildren< Renderer >();
+        foreach( Renderer renderer in rgDescendentRenderers  )
+        {
+            if ( !renderer )
+                continue;
+ 
+            bounds.Encapsulate( renderer.bounds );
+        }
+ 
+        return bounds;
+    }
 }
