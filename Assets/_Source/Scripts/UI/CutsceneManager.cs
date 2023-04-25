be837b4a39db268c9a8bf126b2d490068bf97d7c
Colin Spiridonov <spiridon@usc.edu>
2023-04-24 17:55:15 -0700

    intro day text

--- Assets/_Source/Scripts/UI/CutsceneManager.cs
+++ Assets/_Source/Scripts/UI/CutsceneManager.cs
@@ -3,22 +3,16 @@ using UnityEngine;
 
 public class CutsceneManager : MonoBehaviour {
     public bool playInEditor;
-    public List<Cutscene> cutscenes;
+    public Cutscene cutscene;
     
     // Start is called before the first frame update
     void Start() {
-        foreach (Cutscene cutscene in cutscenes) {
-            cutscene.gameObject.SetActive(false);
-        }
+        cutscene.gameObject.SetActive(false);
 
         if (!Application.isEditor || playInEditor) {
             int currentDay = PlayerPrefs.GetInt("CurrentDay", 1);
         
-            int index = currentDay - 1;
-            // if don't have a cutscene, just play first cutscene
-            if (index > cutscenes.Count) index = 0;
-
-            cutscenes[index].gameObject.SetActive(true);
+            cutscene.gameObject.SetActive(true);
         }
     }
 }
